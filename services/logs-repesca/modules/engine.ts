2dde03c9376d22775279253bba941b6f494a3bce
Jose <4388316+bixus@users.noreply.github.com>
2023-11-03 13:32:16 +0100

    Test

--- services/logs-repesca/modules/engine.ts
+++ services/logs-repesca/modules/engine.ts
@@ -1,14 +1,14 @@
 import {EngineBase} from "services-comun/modules/engine_base";
 
 import {Configuracion} from "./utiles/config";
-import {Repesca} from "./data/source/repesca";
+import {Repesca} from "./data/repesca";
 
 export class Engine extends EngineBase<Configuracion> {
     /* STATIC */
 
     /* INSTANCE */
     public override async ejecutar(): Promise<void> {
-        await Repesca.run();
+        await Repesca.run(this.configuracion);
 
         await super.ejecutar();
     }
