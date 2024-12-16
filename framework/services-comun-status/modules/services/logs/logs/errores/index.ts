6f6a725ba232f3dce686574b8c21c7a653027b10
Juan C. Martínez <jcmartinez@meteored.com>
2024-12-16 09:57:16 +0100

    Framework.

---
+++ framework/services-comun-status/modules/services/logs/logs/errores/index.ts
@@ -0,0 +1,81 @@
+import {BackendRequest, RequestResponse} from "services-comun/modules/net/request-backend";
+import {EService, SERVICES} from "../../../config";
+import {logRejection} from "services-comun/modules/decorators/metodo";
+import {IListOUT} from "./list/interface";
+import {IAvaliableFiltersOUT} from "./available-filters/interface";
+
+interface ListFilter {
+    projects: string[];
+    services?: string[];
+    files?: string[];
+    lines?: number[];
+    urls?: string[];
+    tsFrom?: number;
+    tsTo?: number;
+}
+
+interface ListPagination {
+    page?: number;
+    perPage?: number;
+}
+
+export default class Index extends BackendRequest {
+    /* STATIC */
+    private static SERVICIO = SERVICES.servicio(EService.logs).base;
+
+    @logRejection(true)
+    public static async list(filters: ListFilter, pagination: ListPagination): Promise<RequestResponse<IListOUT>> {
+        let url = `${this.SERVICIO}/private/logs/error/list/`;
+
+        const {projects} = filters;
+        const {page, perPage} = pagination;
+
+        const params: string[] = [];
+        params.push(`projects=${projects.join(';')}`);
+
+        if (filters.services !== undefined && filters.services.length > 0) {
+            params.push(`services=${filters.services.join(';')}`);
+        }
+
+        if (filters.files !== undefined && filters.files.length > 0) {
+            params.push(`files=${filters.files.join(';')}`);
+        }
+
+        if (filters.lines !== undefined && filters.lines.length > 0) {
+            params.push(`lines=${filters.lines.join(';')}`);
+        }
+
+        if (filters.urls !== undefined && filters.urls.length > 0) {
+            params.push(`urls=${filters.urls.join(';')}`);
+        }
+
+        if (filters.tsFrom) {
+            params.push(`ts_from=${filters.tsFrom}`);
+        }
+
+        if (filters.tsTo) {
+            params.push(`ts_to=${filters.tsTo}`);
+        }
+
+        if (page) {
+            params.push(`page=${page}`);
+            if (perPage) {
+                params.push(`perPage=${perPage}`);
+            }
+        }
+
+        if (params.length > 0) {
+            url += `?${params.join('&')}`;
+        }
+
+        return await this.get<IListOUT>(url);
+    }
+
+    @logRejection(true)
+    public static async availableFilters(projects: string[]): Promise<RequestResponse<IAvaliableFiltersOUT>> {
+        const url = `${this.SERVICIO}/private/logs/error/available-filters/?projects=${projects.join(';')}`;
+        return await this.get<IAvaliableFiltersOUT>(url);
+    }
+
+    /* INSTANCE */
+}
--- framework/services-comun-status/modules/services/status-backend/log/index.ts
+++ framework/services-comun-status/modules/services/status-backend/log/index.ts
@@ -2,6 +2,7 @@ import {BackendRequest, RequestResponse} from "services-comun/modules/net/reques
 import {logRejection} from "services-comun/modules/decorators/metodo";
 import {IUserLogServicesOUT} from "./user-log-services/interface";
 import {EService, SERVICES} from "../../config";
+import {IUserLogErrorsOUT} from "./user-log-errors/interface";
 
 export default class Index extends BackendRequest {
     /* STATIC */
@@ -10,6 +11,13 @@ export default class Index extends BackendRequest {
     @logRejection(true)
     public static async userLogServices(token: string): Promise<RequestResponse<IUserLogServicesOUT>> {
         return await this.get<IUserLogServicesOUT>(`${this.SERVICIO}/backend/log/user-log-services`, {
+            auth: token
+        });
+    }
+
+    @logRejection(true)
+    public static async userLogErrors(token: string): Promise<RequestResponse<IUserLogErrorsOUT>> {
+        return await this.get<IUserLogErrorsOUT>(`${this.SERVICIO}/backend/log/user-log-errors`, {
             auth: token
         });
     }
