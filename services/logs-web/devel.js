5bb474d8208d896d0fbd65b0a4ea0fb22981c9e8
Bixus <4388316+bixus@users.noreply.github.com>
2026-09-08 08:24:55 +0200

    Limpieza de workspace

--- services/logs-web/devel.js
+++
@@ -1,2 +0,0 @@
-process.env.TZ ??= 'UTC';
-require("./app");
--- services/logs-web/main.ts
+++
@@ -1,6 +0,0 @@
-import {Main} from "@mr/core-workload";
-
-import {Configuracion} from "./modules/utiles/config";
-import {Engine} from "./modules/engine";
-
-Main.ejecutar(Engine, Configuracion);
